845ef4731191b25d6fb2e0ecfba2a41d0f9d3cb1
shanyuxiyun <lidong198609@hotmail.com>
2016-01-17 19:54:03 +0800

    Update

--- python/examples/AllTk.py
+++ python/examples/AllTk.py
@@ -1,6 +1,8 @@
 from Tkinter import *
 
+
 class AllTkinterWidgets:
+
     def __init__(self, master):
         '''
         The Frame widget is very important for the process of grouping and organizing other widgets in a somehow friendly way. It works like a container, which is responsible for arranging the position of other widgets.
@@ -8,86 +10,97 @@ class AllTkinterWidgets:
         # bd  The size of the border around the indicator. Default is 2 pixels.
         # master  This represents the parent window.
         frame = Frame(master, width=500, height=400, bd=5)
-        #fill: Determines whether widget fills any extra space allocated to it by the packer, or keeps its own minimal dimensions: NONE (default), X (fill only horizontally), Y (fill only vertically), or BOTH (fill both horizontally and vertically).
-        frame.pack(fill = X)
-    # The relief style of a widget refers to certain simulated 3-D effects around the outside of the widget. 
+        # fill: Determines whether widget fills any extra space allocated to it
+        # by the packer, or keeps its own minimal dimensions: NONE (default), X
+        # (fill only horizontally), Y (fill only vertically), or BOTH (fill
+        # both horizontally and vertically).
+        frame.pack(fill=X)
+    # The relief style of a widget refers to certain simulated 3-D effects around the outside of the widget.
     # flat, groove, raised, ridge, solid, or sunken
-        self.mbar = Frame(frame, relief = 'sunken', bd=2)
-        self.mbar.pack(fill = X)
+        self.mbar = Frame(frame, relief='sunken', bd=2)
+        self.mbar.pack(fill=X)
 
     # Create File menu
-        self.filebutton = Menubutton(self.mbar, text = 'File')
-    # Determines which side of the parent widget packs against: TOP (default), BOTTOM, LEFT, or RIGHT.
-        self.filebutton.pack(side = LEFT)
+        self.filebutton = Menubutton(self.mbar, text='File')
+    # Determines which side of the parent widget packs against: TOP (default),
+    # BOTTOM, LEFT, or RIGHT.
+        self.filebutton.pack(side=LEFT)
 
-    # Normally, a menu can be torn off, the first position (position 0) in the list of choices is occupied by the tear-off element, and the additional choices are added starting at position 1. If you set tearoff=0, the menu will not have a tear-off feature, and choices will be added starting at position 0.
+    # Normally, a menu can be torn off, the first position (position 0) in the
+    # list of choices is occupied by the tear-off element, and the additional
+    # choices are added starting at position 1. If you set tearoff=0, the menu
+    # will not have a tear-off feature, and choices will be added starting at
+    # position 0.
         self.filemenu = Menu(self.filebutton, tearoff=0)
-    # menu To associate the menubutton with a set of choices, set this option to the Menu object containing those choices. That menu object must have been created by passing the associated menubutton to the constructor as its first argument.
-        self.filebutton['menu']=self.filemenu
+    # menu To associate the menubutton with a set of choices, set this option
+    # to the Menu object containing those choices. That menu object must have
+    # been created by passing the associated menubutton to the constructor as
+    # its first argument.
+        self.filebutton['menu'] = self.filemenu
 
     # Populate File menu
-        self.filemenu.add('command', label = 'Exit', command = self.quit)
-        self.filemenu.add('command', label = 'Save', command = self.quit)
-        
-        
+        self.filemenu.add('command', label='Exit', command=self.quit)
+        self.filemenu.add('command', label='Save', command=self.quit)
+
     # Create  object menu
-        self.objectbutton = Menubutton(self.mbar, text = 'Object', )
-        self.objectbutton.pack(side = LEFT)
+        self.objectbutton = Menubutton(self.mbar, text='Object', )
+        self.objectbutton.pack(side=LEFT)
 
         self.objectmenu = Menu(self.objectbutton, tearoff=0)
         self.objectbutton['menu'] = self.objectmenu
 
     # Populate object menu
-        self.objectmenu.add('command', label = 'object', command = self.stub)
+        self.objectmenu.add('command', label='object', command=self.stub)
 
     # Create  edit menu
-        self.editbutton = Menubutton(self.mbar, text = 'Edit', )
-        self.editbutton.pack(side = LEFT)
+        self.editbutton = Menubutton(self.mbar, text='Edit', )
+        self.editbutton.pack(side=LEFT)
 
         self.editmenu = Menu(self.editbutton, tearoff=0)
         self.editbutton['menu'] = self.editmenu
 
     # Populate edit menu
-        self.editmenu.add('command', label = 'edit', command = self.stub)
+        self.editmenu.add('command', label='edit', command=self.stub)
 
     # Create  view menu
-        self.viewbutton = Menubutton(self.mbar, text = 'View', )
-        self.viewbutton.pack(side = LEFT)
+        self.viewbutton = Menubutton(self.mbar, text='View', )
+        self.viewbutton.pack(side=LEFT)
 
         self.viewmenu = Menu(self.viewbutton, tearoff=0)
         self.viewbutton['menu'] = self.viewmenu
 
     # Populate view menu
-        self.viewmenu.add('command', label = 'view', command = self.stub)
+        self.viewmenu.add('command', label='view', command=self.stub)
 
     # Create  tools menu
-        self.toolsbutton = Menubutton(self.mbar, text = 'Tools', )
-        self.toolsbutton.pack(side = LEFT)
+        self.toolsbutton = Menubutton(self.mbar, text='Tools', )
+        self.toolsbutton.pack(side=LEFT)
 
         self.toolsmenu = Menu(self.toolsbutton, tearoff=0)
         self.toolsbutton['menu'] = self.toolsmenu
 
     # Populate tools menu
-        self.toolsmenu.add('command', label = 'tools', command = self.stub)
+        self.toolsmenu.add('command', label='tools', command=self.stub)
 
     # Create  help menu
-        self.helpbutton = Menubutton(self.mbar, text = 'Help', )
-        self.helpbutton.pack(side = RIGHT)
+        self.helpbutton = Menubutton(self.mbar, text='Help', )
+        self.helpbutton.pack(side=RIGHT)
 
         self.helpmenu = Menu(self.helpbutton, tearoff=0)
         self.helpbutton['menu'] = self.helpmenu
 
     # Populate help menu
-        self.helpmenu.add('command', label = 'help', command = self.stub)
+        self.helpmenu.add('command', label='help', command=self.stub)
 
         iframe1 = Frame(frame, bd=2, relief=SUNKEN)
-        
+
         # padx Additional padding left and right of the text.
         Button(iframe1, text='Button').pack(side=LEFT, padx=5)
         Checkbutton(iframe1, text='CheckButton').pack(side=LEFT, padx=5)
 
-        #Anchors are used to define where text is positioned relative to a reference point
-        v=IntVar()
+        # Anchors are used to define where text is positioned relative to a
+        # reference point
+        v = IntVar()
         Radiobutton(iframe1, text='Button', variable=v,
                     value=3).pack(side=RIGHT, anchor=W)
         Radiobutton(iframe1, text='Dio', variable=v,
@@ -105,14 +118,15 @@ class AllTkinterWidgets:
 
         iframe3 = Frame(frame, bd=2, relief=GROOVE)
         listbox = Listbox(iframe3, height=4)
-        for line in ['Listbox Entry One','Entry Two','Entry Three','Entry Four']:
+        for line in ['Listbox Entry One',
+                     'Entry Two', 'Entry Three', 'Entry Four']:
             listbox.insert(END, line)
         listbox.pack(fill=X, padx=5)
         iframe3.pack(expand=1, fill=X, pady=10, padx=5)
-        
+
         iframe4 = Frame(frame, bd=2, relief=SUNKEN)
-        #The Text widget is used to display text in multiple lines.
-        text=Text(iframe4, height=10, width =65)
+        # The Text widget is used to display text in multiple lines.
+        text = Text(iframe4, height=10, width=65)
         fd = open('guido.txt')
         lines = fd.read()
         fd.close()
@@ -122,7 +136,7 @@ class AllTkinterWidgets:
         # Methods inherited from YView:
         #yview(self, *args)
         # Query and change the vertical position of the view.
-        #command  A procedure to be called whenever the scrollbar is moved.
+        # command  A procedure to be called whenever the scrollbar is moved.
         sb = Scrollbar(iframe4, orient=VERTICAL, command=text.yview)
         sb.pack(side=RIGHT, fill=Y)
         text.configure(yscrollcommand=sb.set)
@@ -134,13 +148,16 @@ class AllTkinterWidgets:
         c = Canvas(iframe5, bg='white', width=340, height=100)
         c.pack()
         for i in range(25):
-            c.create_oval(5+(4*i),5+(3*i),(5*i)+60,(i)+60, fill='gray70')
+            c.create_oval(5 + (4 * i), 5 + (3 * i), (5 * i) +
+                          60, (i) + 60, fill='gray70')
         c.create_text(260, 80, text='Canvas', font=('verdana', 10, 'bold'))
         iframe5.pack(expand=1, fill=X, pady=10, padx=5)
-        
+
         iframen = Frame(frame, bd=2, relief=FLAT)
-        #This widget provides a multiline and noneditable object that displays texts, automatically breaking lines and justifying their contents.
-        #Its functionality is very similar to the one provided by the Label widget, except that it can also automatically wrap the text, maintaining a given width or aspect ratio.
+        # This widget provides a multiline and noneditable object that displays texts, automatically breaking lines and justifying their contents.
+        # Its functionality is very similar to the one provided by the Label
+        # widget, except that it can also automatically wrap the text,
+        # maintaining a given width or aspect ratio.
         Message(iframen, text='This is a Message widget', width=300,
                 relief=SUNKEN).pack(fill=X, padx=5)
         iframen.pack(expand=1, fill=X, pady=10, padx=5)
@@ -150,7 +167,7 @@ class AllTkinterWidgets:
 
     def stub(self):
         pass
-    
+
 root = Tk()
 root.option_add('*font', ('verdana', 10, 'bold'))
 all = AllTkinterWidgets(root)
--- python/tools/codingchecker.py
+++ python/tools/codingchecker.py
@@ -10,50 +10,56 @@ import zipfile
 import shutil
 from datetime import datetime
 
+
 class CodingChecker(object):
-    
-    CHECKED_FILE_EXT = ['.sql', '.zip','.py']
-    
+
+    CHECKED_FILE_EXT = ['.sql', '.zip', '.py']
+
     def __init__(self, checked_file=None):
         self.checked_file = checked_file
-        if self.checked_file == None :
+        if self.checked_file is None:
             self.checked_file = os.path.abspath('.')
         self.counter = 0
         if os.path.exists(self.checked_file):
             if os.path.isdir(self.checked_file):
                 self.checked_file = os.path.abspath(checked_file)
-                self.tempdir = os.path.join(self.checked_file, datetime.now().strftime('%Y%m%d%H%M%S%f'))
-            else :
-                self.tempdir = os.path.join(os.path.abspath(os.path.dirname(self.checked_file)), datetime.now().strftime('%Y%m%d%H%M%S%f'))
-            
-            logger_file = self.tempdir+'.log'
+                self.tempdir = os.path.join(
+                    self.checked_file, datetime.now().strftime('%Y%m%d%H%M%S%f'))
+            else:
+                self.tempdir = os.path.join(
+                    os.path.abspath(
+                        os.path.dirname(
+                            self.checked_file)),
+                    datetime.now().strftime('%Y%m%d%H%M%S%f'))
+
+            logger_file = self.tempdir + '.log'
             if os.path.exists(logger_file):
                 os.remove(logger_file)
-            self.logger = open(logger_file,'a')
-        else :
+            self.logger = open(logger_file, 'a')
+        else:
             raise BaseException('Param Wrong.')
-    
+
     def _cur_tempdir(self):
         curpath = os.path.join(self.tempdir, str(self.counter))
         if not os.path.exists(curpath):
             print 'start to make dir %s' % curpath
             os.makedirs(curpath)
         return curpath
-    
+
     def __del__(self):
         if self.counter > 0:
             for i in range(self.counter + 1):
                 if os.path.exists(os.path.join(self.tempdir, str(i))):
                     shutil.rmtree(os.path.join(self.tempdir, str(i)))
             shutil.rmtree(self.tempdir)
-    
+
     def check(self):
         if not os.path.exists(self.checked_file):
             print 'Error -> path {%s} does not exist.' % self.checked_file
             return
         self._check_file(self.checked_file)
         self.logger.close()
-    
+
     def _check_zip_file(self, zip_file):
         print 'start to extract zip file -> %s' % zip_file
         self.counter = self.counter + 1
@@ -61,40 +67,41 @@ class CodingChecker(object):
         z.extractall(path=self._cur_tempdir())
         z.close()
         self._check_dir(self._cur_tempdir())
-        
-    
+
     def _is_need_checked(self, fl):
-        return os.path.splitext(fl)[1].lower() in CodingChecker.CHECKED_FILE_EXT
-    
+        return os.path.splitext(fl)[1].lower(
+        ) in CodingChecker.CHECKED_FILE_EXT
+
     def _check_single_file(self, fl):
         print fl
-        encoding =  chardet.detect(open(fl).read())
+        encoding = chardet.detect(open(fl).read())
         print encoding
         if encoding['encoding'] != 'utf-8':
-            self.logger.write('%s\t%s\t%f;' % (fl,encoding['encoding'],encoding['confidence']))
-    
+            self.logger.write(
+                '%s\t%s\t%f;' %
+                (fl, encoding['encoding'], encoding['confidence']))
+
     def _check_file(self, fl):
         if os.path.isfile(fl):
             if self._is_need_checked(fl):
                 if zipfile.is_zipfile(fl):
                     self._check_zip_file(fl)
-                else :
+                else:
                     self._check_single_file(fl)
-            else :
+            else:
                 print 'ignore to check file -> %s' % fl
         elif os.path.isdir(fl):
             self._check_dir(fl)
-    
-    
+
     def _check_dir(self, dirpath):
         root, dirs, files = os.walk(dirpath).next()
         for sub_file in files:
             self._check_file(os.path.join(root, sub_file))
         for sub_dir in dirs:
             self._check_file(os.path.join(root, sub_dir))
-    
-    def _log_result(self,content):
-        self.logger.write(content+'\r\n')
+
+    def _log_result(self, content):
+        self.logger.write(content + '\r\n')
 
 if __name__ == '__main__':
     cc = CodingChecker('./test')
