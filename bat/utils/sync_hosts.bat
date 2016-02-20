c64f58f5234d29169e4f316c5607f6055249732b
shanyuxiyun <lidong198609@hotmail.com>
2016-02-20 19:51:47 +0800

    Update

--- bat/utils/sync_hosts.bat
+++ bat/utils/sync_hosts.bat
@@ -1,11 +1,11 @@
-@echo on
-set CURRENT=%CD%
-set HOME=E:\Liz\repos\git\hosts
+@echo off
+set HOME=%CD%
 set HOSTS_PATH=C:\WINDOWS\system32\drivers\etc
-cd /d %HOME%
-git pull
+set CURL_CMD=%HOME%\download\curl.exe
+
+%CURL_CMD% -k -# -O https://raw.githubusercontent.com/racaljk/hosts/master/hosts
 copy %HOSTS_PATH%\hosts %HOSTS_PATH%\hosts.bak
 DEL %HOSTS_PATH%\hosts
 copy hosts %HOSTS_PATH%
 
-CD %CURRENT%
+CD %HOME%
