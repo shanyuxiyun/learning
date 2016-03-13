61e65594581957e33001b29369a2272c69b26239
shanyuxiyun <lidong198609@hotmail.com>
2016-03-13 09:53:24 +0800

    Update

--- bat/utils/sync_hosts.bat
+++ bat/utils/sync_hosts.bat
@@ -1,11 +1,12 @@
 @echo off
-set HOME=%CD%
+set WORK_DIR=%CD%
 set HOSTS_PATH=C:\WINDOWS\system32\drivers\etc
-set CURL_CMD=%HOME%\download\curl.exe
+set CURL_CMD=%WORK_DIR%\download\curl.exe
 
 %CURL_CMD% -k -# -O https://raw.githubusercontent.com/racaljk/hosts/master/hosts
 copy %HOSTS_PATH%\hosts %HOSTS_PATH%\hosts.bak
 DEL %HOSTS_PATH%\hosts
 copy hosts %HOSTS_PATH%
+DEL hosts
 
-CD %HOME%
+CD %WORK_DIR%
